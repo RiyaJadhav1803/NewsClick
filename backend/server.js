53cee8284d2bd753ee529d258e11eaf5dce941fd
RiyaJadhav1803 <166241201+RiyaJadhav1803@users.noreply.github.com>
2024-07-15 11:57:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const cors=require("cors");
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://newsclick-front.onrender.com/",
     credentials: true
   }));
 
