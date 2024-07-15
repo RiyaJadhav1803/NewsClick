9ad9a6a1aa72acbf42be08f4e6b51c7cf18c3667
RiyaJadhav1803 <166241201+RiyaJadhav1803@users.noreply.github.com>
2024-07-15 19:33:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(function(req,res,next){
   res.header("Access-Control-Allow-Origin" , "https://newsclick-frontend.onrender.com");
   res.header("Access-Control-Allow-Methods" , "GET,POST,HEAD,OPTIONS,PUT,DELETE");
-   res.header("Access-Control-Allow-Headers" , "Origin",X-Requested-With, Content-Type , Accept , Authorization");
+   res.header("Access-Control-Allow-Headers" , "Origin,X-Requested-With, Content-Type , Accept , Authorization");
   next();
 });
 
